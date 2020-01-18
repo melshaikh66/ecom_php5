7b8b032b019bfc56e9db43b5c957b0286a97f226
Mostafa Elshaikh <m.elshaikh66@gmail.com>
2020-01-18 13:26:29 +0300

    add items page

--- admin/items.php
+++ admin/items.php
@@ -25,7 +25,7 @@ if (isset($_SESSION['username'])) {
         <div class="form-group row">
             <label for="" class="col-form-label control-label col-sm-3 text-capitalize">Name</label>
             <div class="col-9 col-md-6">
-                <input type="text" name="name" class="form-control" required placeholder="Name of the item" />
+                <input type="text" name="name" class="form-control"  placeholder="Name of the item" />
             </div>
         </div>
         <!-- end name feild -->
@@ -33,7 +33,7 @@ if (isset($_SESSION['username'])) {
         <div class="form-group row">
             <label for="" class="col-form-label control-label col-sm-3 text-capitalize">Description</label>
             <div class="col-9 col-md-6">
-                <input type="text" name="description" class="form-control" required placeholder="Descripe the item" />
+                <input type="text" name="description" class="form-control" placeholder="Descripe the item" />
             </div>
         </div>
         <!-- end Description feild -->
@@ -41,7 +41,7 @@ if (isset($_SESSION['username'])) {
         <div class="form-group row">
             <label for="" class="col-form-label control-label col-sm-3 text-capitalize">Price</label>
             <div class="col-9 col-md-6">
-                <input type="text" name="price" class="form-control" required placeholder="Price the item" />
+                <input type="text" name="price" class="form-control" placeholder="Price the item" />
             </div>
         </div>
         <!-- end Price feild -->
@@ -49,7 +49,7 @@ if (isset($_SESSION['username'])) {
         <div class="form-group row">
             <label for="" class="col-form-label control-label col-sm-3 text-capitalize">Country</label>
             <div class="col-9 col-md-6">
-                <input type="text" name="country" class="form-control" required  placeholder="Country the item" />
+                <input type="text" name="country" class="form-control"  placeholder="Country the item" />
             </div>
         </div>
         <!-- end Country feild -->
@@ -67,6 +67,45 @@ if (isset($_SESSION['username'])) {
             </div>
         </div>
         <!-- end Status feild -->
+          <!-- start members feild -->
+        <div class="form-group row">
+            <label for="" class="col-form-label control-label col-sm-3 text-capitalize">Member</label>
+            <div class="col-9 col-md-6">
+               <select name="member" id="">
+                   <option value="0">...</option>
+          			<?php 
+          			$stmt = $con->prepare("SELECT * FROM users");
+          			$stmt->execute();
+          			$users = $stmt->fetchAll();
+          			
+          			foreach ($users as $user) {
+          			    echo "<option value='" .$user['UserID'] ."'>". $user['Username']."</option>";
+          			}
+          			
+          			?>
+               </select>
+            </div>
+        </div>
+        <!-- end members feild -->
+          <!-- start categories feild -->
+        <div class="form-group row">
+            <label for="" class="col-form-label control-label col-sm-3 text-capitalize">Category</label>
+            <div class="col-9 col-md-6">
+               <select name="category" id="">
+                   <option value="0">...</option>
+          			<?php 
+          			$stmt = $con->prepare("SELECT * FROM categories");
+          			$stmt->execute();
+          			$cats = $stmt->fetchAll();
+          			
+          			foreach ($cats as $cat) {
+          			    echo "<option value='" .$cat['ID'] ."'>". $cat['Name']."</option>";
+          			}
+          			?>
+               </select>
+            </div>
+        </div>
+        <!-- end categories feild -->
         <!-- start submit feild -->
         <div class="form-group row">
             <div class="offset-3 col-sm-10">
@@ -75,9 +114,89 @@ if (isset($_SESSION['username'])) {
         </div>
         <!-- end submit feild -->
     </form>
-</div>
+</div>  
 <?php
-} elseif ($do == "insert") {
+} elseif ($do == "insert") { // insert page 
+   
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        echo "<h1 class='text-center'>Add Member</h1>";
+        echo "<div class ='container'>";
+        
+        //get variables from form
+        $name       = $_POST['name'];
+        $desc       = $_POST['description'];
+        $price      = $_POST['price'];
+        $country    = $_POST['country'];
+        $status     = $_POST['status'];
+        $member     = $_POST['member'];
+        $cat        = $_POST['category'];
+       
+        // Validate the form
+        
+        $formErrors = array();
+      
+        if (empty($name)) {
+            $formErrors[] = " Name can't be <strong>Empty</strong>";
+        }
+        if (empty($desc)) {
+            $formErrors[] = " Description can't be <strong>Empty</strong>";
+            
+        }
+        
+        if (empty($price)) {
+            $formErrors[] = " Price can't be <strong>Empty</strong>";
+            
+        }
+        
+        if (empty($country)) {
+            $formErrors[] = "Country can't be <strong>Empty</strong>";
+            
+        }
+        if ($status == 0) {
+            $formErrors[] = "You must choose <strong>Status</strong>";
+            
+        }
+        if ($member == 0) {
+            $formErrors[] = "You must choose <strong>Member</strong>";
+            
+        }
+        if ($cat == 0) {
+            $formErrors[] = "You must choose <strong>Category</strong>";
+            
+        }
+        // loop into error array and echo it
+        foreach ($formErrors as $error) {
+            echo "<div class='alert alert-danger'>" . $error . "</div>";
+        }
+        
+        // check if there is no errors update the database
+        if (empty($formErrors)) {
+            
+                // insert the user info to the database
+                $stmt = $con->prepare("INSERT INTO items(Name, Description, Price, Country,Status, Add_Date, Cat_ID, Member_ID)
+                                                VALUES(:zname, :zdesc, :zprice, :zcountry, :zstatus, now(), :zcat, :zmember)");
+                $stmt->execute(array(
+                    "zname"     => $name,
+                    "zdesc"     => $desc,
+                    "zprice"    => $price,
+                    "zcountry"  => $country,
+                    "zstatus"   => $status,
+                    "zcat"      => $cat,
+                    "zmember"   => $member,
+                ));
+                // echo success message
+                
+                $theMsg = "<div class='alert alert-success'>" . $stmt->rowCount() . " Records Inserted</div>";
+                redirectHome($theMsg, "back");
+            
+        }
+    } else {
+        echo "<div class='container'>";
+        $theMsg = "<div class='alert alert-danger'>You can't browse this page directly</div>";
+        redirectHome($theMsg);
+        echo "</div>";
+    }
+    echo "</div>";
 
     } elseif ($do == "edit") {
 
@@ -99,3 +218,6 @@ if (isset($_SESSION['username'])) {
 }
 
 ob_end_flush();
+
+?>
+
